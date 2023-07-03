d058aedd2f6c4d7696242d55e862a327990c9a20
TylerJeman <tyler.s.jerman@gmail.com>
2023-07-03 08:58:45 -0600

    finished

---
+++ javascript-2/practice-js-2.js
@@ -0,0 +1,106 @@
+/*
+    For this section of the assessment you will be putting together a cheat sheet for common git commands.  
+    You'll provide the command as well as what it does. 
+*/
+
+//////////////////PROBLEM 1////////////////////
+/*
+    Create a variable called 'gitDefinition'.
+    It should be a string containing your best definition of what Git is.
+*/
+
+//CODE HERE
+var gitDefinition = "git is a control system.";
+//////////////////PROBLEM 2////////////////////
+/*
+    Create a variable called 'gitHubDefinition'.  
+    It should be a string containing your best definition of what GitHub is.
+*/
+
+//CODE HERE
+ var gitHubDefinition = "gitHub is a website used to store code.";
+//////////////////PROBLEMS 3 - 9////////////////////
+/*
+    For the next several problems you will be creating objects containing information about different git 
+    commands.  Each object should contain 'description' and 'code' properties.  The 'description' property 
+    will be a string with a description of what that git command does.  The 'code' property should be a 
+    string of the actual command (what you type into your terminal).
+*/
+
+//////////////////PROBLEM 3////////////////////
+/*
+    Create an object called 'init' with 'description' and 'code' properties 
+    following the guidelines above to describe the init command.
+*/
+
+//CODE HERE
+ var init = {
+    description: "Initialize a new git repository.",
+    code: "git init"
+};
+//////////////////PROBLEM 4////////////////////
+/*
+    Create an object called 'clone' with 'description' and 'code' properties 
+    following the guidelines above to describe the clone command.
+*/
+
+//CODE HERE
+ var clone = {
+    description: "Clone a git repository.",
+    code: "git clone"
+};
+//////////////////PROBLEM 5////////////////////
+/*
+    Create an object called 'status' with 'description' and 'code' properties 
+    following the guidelines above to describe the status command.
+*/
+
+//CODE HERE
+ var status = {
+    description: "Check the status of a git repository.",
+    code: "git status"
+};
+//////////////////PROBLEM 6////////////////////
+/*
+    Create an object called 'add' with 'description' and 'code' properties 
+    following the guidelines above to describe the add command.
+*/
+
+//CODE HERE
+ let add = {
+    description: "Add files to a git repository.",
+    code: "git add"
+};
+//////////////////PROBLEM 7////////////////////
+/*
+    Create an object called 'commit' with 'description' and 'code' properties
+    following the guidelines above to describe the commit command.
+*/
+
+//CODE HERE
+    var commit = {
+        description: "Commit changes to a git repository.",
+        code: "git commit"
+    };
+//////////////////PROBLEM 8////////////////////
+/*
+    Create an object called 'addRemote' with 'description' and 'code' properties
+    following the guidelines above to describe the command to add a remote location to your git repository.
+*/
+
+//CODE HERE
+ var addRemote = {
+    description: "Add a remote location to your git repository.",
+    code: "git remote add origin"
+};
+//////////////////PROBLEM 9////////////////////
+/*
+    Create an object called 'push' with 'description' and 'code' properties 
+    following the guidelines above to describe the push command.
+*/
+
+//CODE HERE
+ var push = {
+    description: "Push changes to a git repository.",
+    code: "git push"
+};
